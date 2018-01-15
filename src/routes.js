4c2fc2073a956b59eb7c6849525fe080920d5f31
dliszka <ddewid@gmail.com>
2018-01-15 02:22:33 +0100

    Added stat route declaration in global routes.

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Router } from 'express'
 import Authenticate from './middleware/authenticate'
 import authRoutes from './routes/auth'
 import roomRoutes from './routes/room'
+import statRoutes from './routes/stat'
 
 const router = Router();
 
 router.use('/auth', authRoutes)
 router.use('/room', Authenticate, roomRoutes)
+router.use('/stat', Authenticate, statRoutes)
 
 export default router
